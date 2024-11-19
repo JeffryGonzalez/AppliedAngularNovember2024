2570e61011320481f10515b61a1aa70427626aad
Jeffry Gonzalez <jeff@hypertheory.com>
2024-11-19 16:11:28 -0500

    added devtools

--- src/frontend/src/app/features/gifts/services/people.store.ts
+++ src/frontend/src/app/features/gifts/services/people.store.ts
@@ -1,8 +1,10 @@
 import { patchState, signalStore, withMethods } from '@ngrx/signals';
 import { addEntity, withEntities } from '@ngrx/signals/entities';
 import { PeopleCreate, PeopleEntity } from '../types';
+import { withDevtools } from '@angular-architects/ngrx-toolkit';
 
 export const PeopleStore = signalStore(
+  withDevtools('people-store'),
   withEntities<PeopleEntity>(),
   withMethods((store) => {
     // injection context
