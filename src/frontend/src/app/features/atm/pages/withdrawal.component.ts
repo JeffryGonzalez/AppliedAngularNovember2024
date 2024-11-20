69bd22d8f918df00d0063adbd871226c4687db25
Jeffry Gonzalez <jeff@hypertheory.com>
2024-11-20 11:33:54 -0500

    withdrawal pretty done

--- src/frontend/src/app/features/atm/pages/withdrawal.component.ts
+++ src/frontend/src/app/features/atm/pages/withdrawal.component.ts
@@ -1,10 +1,32 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { WITHDRAWAL_AMOUNTS, WithdrawAmount } from '../types';
 
 @Component({
   selector: 'app-atm-withdrawal',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <p>Withdrawal</p> `,
+  imports: [CurrencyPipe],
+  template: `
+    <p>Your Balance is {{ balance() | currency }}</p>
+    <div class="grid grid-flow-col gap-8">
+      @for (amt of amounts; track amt) {
+        <button
+          [disabled]="balance() < amt"
+          (click)="withdraw(amt)"
+          class="btn btn-primary ring-2 ring-white"
+        >
+          {{ amt | currency }}
+        </button>
+      }
+    </div>
+  `,
   styles: ``,
 })
-export class WithdrawalComponent {}
+export class WithdrawalComponent {
+  balance = signal(500.23);
+
+  amounts = WITHDRAWAL_AMOUNTS;
+  withdraw(amount: WithdrawAmount) {
+    this.balance.update((b) => b - amount);
+  }
+}
