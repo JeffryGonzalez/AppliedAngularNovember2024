3b91ed90c4dce938a91c0b1113e0b3c710fac589
Jeffry Gonzalez <jeff@hypertheory.com>
2024-11-20 12:05:22 -0500

    done with demo

--- src/frontend/src/app/features/atm/services/atm.store.ts
+++ src/frontend/src/app/features/atm/services/atm.store.ts
@@ -1,7 +1,9 @@
 import {
   patchState,
   signalStore,
+  watchState,
   withComputed,
+  withHooks,
   withMethods,
   withState,
 } from '@ngrx/signals';
@@ -10,7 +12,7 @@ import { computed } from '@angular/core';
 
 export const AtmStore = signalStore(
   withState({
-    balance: 500.23,
+    balance: 0,
     amounts: WITHDRAWAL_AMOUNTS,
   }),
   withMethods((store) => {
@@ -18,11 +20,26 @@ export const AtmStore = signalStore(
       withdraw(amount: WithdrawAmount) {
         patchState(store, { balance: store.balance() - amount });
       },
+      deposit(amount: number) {
+        patchState(store, { balance: store.balance() + amount });
+      },
     };
   }),
   withComputed((store) => {
     return {
       atBalanceThreshold: computed(() => store.balance() < 50),
     };
+  }),
+  withHooks({
+    onInit(store) {
+      const savedBalance = localStorage.getItem('balance');
+
+      if (savedBalance !== null) {
+        patchState(store, { balance: +savedBalance });
+      }
+      watchState(store, (state) => {
+        localStorage.setItem('balance', state.balance.toString());
+      });
+    },
   }),
 );
