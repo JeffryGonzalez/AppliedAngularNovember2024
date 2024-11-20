fa1c9b771de8d7f02a4d32613857c467b05e50c7
Jeffry Gonzalez <jeff@hypertheory.com>
2024-11-20 11:11:08 -0500

    routing for atm

---
+++ src/frontend/src/app/features/atm/atm.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>ATM Goes Here</p> `,
+  styles: ``,
+})
+export class AtmComponent {}
